74e9d06cc0295cab6dcf8a253818f8374fd5ae4b
dev <dev@com.dev.aspire>
2017-04-22 20:34:50 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express');
-var redis = require('redis');
 var app = express();
+var client = require('redis').createClient();
+
+client.on('error', function(err){
+  console.log('Err: '+ err);
+});
 
 app.get('/', function(req, res) {
   res.send("Welcome To Cocktailz backend. Please die!");
 });
 
+app.get('/cocktail/:cocktail', function(req, res){
+  client.hgetall("cocktail:"+req.params.cocktail, function(err, result){
+    console.log("cocktail:"+req.params.cocktail);
+    res.send(JSON.stringify(result));
+  });
+});
+
 app.listen(3000, function() {
   console.log("App listening on port 3000");
-})
+});
