867718ceb98dcca4ba8c783730fe57c9cfad5e54
NightmareEDM <lillykavsek@hotmail.com>
2017-05-08 10:06:54 +0200

    aufgabe

---
+++ aufgabe.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var fs = require('fs');
+var chalk = require('chalk');
+
+fs.readFile("staedte.json", function(err, dat){
+  data = JSON.parse(dat); // parsing filestream to json
+  var cities = dat.cities;
+
+  // TODO EXTERNALIZE FUNCTION
+  cities.sort(function(a, b) {
+    return a.population - b.population;
+  });
+
+  var stringified = JSON.stringify(cities);
+  fs.writeFile("updatedcities.json", stringified, (err) => {
+    if (err) throw err;
+    console.log("ERROR: SUCCESS!");
+  });
+
+  getData(cities);
+
+});
+
+function getData(dat) {
+  for (var i=0; i<data.length; i++) {
+    console.log(chalk.red("Name: ", dat[i].name));
+    console.log(chalk.green("Name: ", dat[i].country));
+    console.log(chalk.blue("Name: ", dat[i].population));
+    console.log("--------");
+  }
+}
