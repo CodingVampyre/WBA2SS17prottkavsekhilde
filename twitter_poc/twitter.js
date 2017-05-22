8626b7919637ed7a7dfeadf08d49a52108d8fc05
NightmareEDM <lillykavsek@hotmail.com>
2017-05-22 12:09:05 +0200

    update poc

---
+++ twitter_poc/twitter.js
@@ -0,0 +1,45 @@
+'use strict'
+
+var express = require('express');
+var Twit = require('twit');
+var bodyParser = require ('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var T = new Twit({
+  consumer_key: '35OehYnLxTe53lmbilZhKjR1j',
+  consumer_secret: 'eb7iDOqQW96jywKFudoyOs388MR9H3wLCpXHKd4PWODu5NVHz8',
+  access_token: '332316298-elzOdDzXWu9clm0qqIjyJL6l4lTZE2ki0kRBOfVq',
+  access_token_secret: 'eDdLzLdhQMUFzVSRNiRV7SVumdFWc8CkpBQY6UvZeLhhC'
+});
+
+app.get('/', function(req, res){
+  res.send("SUCCESS");
+});
+
+app.get("/twitter/:search", function(req, res){
+
+  T.get('search/tweets', {q: req.params.search, count: 20}, function(err, data, response){
+    if (err) console.log(err);
+
+    var emp = "";
+    var statuses = data.statuses;
+    var test = statuses[1].text;
+    console.log(test);
+
+    for (var i=0; i<statuses.length; i++) {
+      emp += "<p>" + statuses[i].text + "</p><br>";
+    }
+
+    console.log(emp);
+
+    res.send(emp);
+  });
+
+});
+
+app.listen(3000, function(){
+  console.log("Server running on Port 3000...");
+});
