c0c7659675ea80f7eecb663391c5beca68be90ff
tprott <tim.prott@freenet.de>
2017-05-26 18:43:37 +0200

    redis added

--- redis_base/clockwork.js
+++ redis_base/clockwork.js
@@ -1,7 +1,10 @@
 var express = require ("express");
 var client = require ("redis").createClient();
-
+var bodyParser = require ("body-parser");
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/test",function(req, res){
   client.hgetall("user:admin", function(err, reply){
@@ -20,7 +23,16 @@ app.listen(3000, '0.0.0.0', function(){
   console.log("Zeit für ein Rein-Raus-Spiel auf Port 3000");
 });
 
-app.get("/gettestenv", (req, res) => {
-  res.send("der Server funktioniert");
-  console.log("der Server funktioniert");
+
+app.post("/new/cocktail", (req, res) => {
+  console.log(req);
+  res.send(JSON.stringify(req.body.cocktail_name));
+
+});
+
+app.get("/list", (req, res) => {
+  client.lrange("test:first","0","-1", (error, reply) => {
+    res.send(JSON.stringify(reply));
+
+  });
 });
