f868e93f541e0ad0dd4081574a237d2ff6639c06
DaneWayne <d.g.hildebrandt@gmail.com>
2017-07-11 18:45:22 +0200

    Bugfixinggggggggg

--- db_server/server.js
+++ db_server/server.js
@@ -116,8 +116,8 @@ app.get("/new/cocktail", (req, res) => {
 // TODO test_required
 app.post("/createnewcocktail", jsonparser, (req, res) => {
 
-  var getSpecificCocktail = "127.0.0.1:"+DIENSTNUTZERPORT+"/cocktail/"+req.body.cocktail_name;
-  var postSpecificCocktail = "127.0.0.1:"+DIENSTNUTZERPORT+"/cocktails";
+  var getSpecificCocktail = "http://127.0.0.1:"+DIENSTNUTZERPORT+"/cocktail/"+req.body.cocktail_name;
+  var postSpecificCocktail = "http://127.0.0.1:"+DIENSTNUTZERPORT+"/cocktails";
   var mymessage = "Hey droogs! There was a BRAND NEW cocktail on our site: /cocktail/" + req.body.cocktail_name;
 
   mytwitter.post('statuses/update', {status: mymessage}, (err, data, response) => {
