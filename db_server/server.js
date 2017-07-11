69d773575c90cb4211c80d416c67ee8c67a09888
NightmareEDM <lillykavsek@hotmail.com>
2017-07-11 16:08:39 +0200

    update server.js

--- db_server/server.js
+++ db_server/server.js
@@ -91,6 +91,8 @@ app.get("/cocktails/:cocktail", jsonparser, (req, res) => {
 
         body2 = JSON.stringify(body2);
 
+        console.log("myingredients(STRINGIFIED: " + body2);
+
         if (!error2) {
           res.render("cocktail.pug", {
             cocktail: body.name,
