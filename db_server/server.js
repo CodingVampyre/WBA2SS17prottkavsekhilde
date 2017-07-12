592215c7e19afe7cefefe73346690c3be0bfb350
NightmareEDM <lillykavsek@hotmail.com>
2017-07-12 18:06:42 +0200

    update server.js

--- db_server/server.js
+++ db_server/server.js
@@ -127,7 +127,7 @@ app.post("/createnewcocktail", jsonparser, (req, res) => {
 
   mytwitter.post('statuses/update', {status: mymessage}, (err, data, response) => {
 
-    console.log("Ingr: " + JSON.stringify(req.body.ingr));
+    console.log("Ingr: " + JSON.parse(req.body.ingr));
 
     var myform = {url: postSpecificCocktail, form: req.body};
 
