1352e03aa1adcf981e6448b80aa5aa5a9982c357
NightmareEDM <lillykavsek@hotmail.com>
2017-07-12 18:42:32 +0200

    update server.js

--- db_server/server.js
+++ db_server/server.js
@@ -139,7 +139,7 @@ app.post("/createnewcocktail", jsonparser, (req, res) => {
       var newing = "http//127.0.0.1:" + DIENSTNUTZERPORT + "/cocktails/" + req.body.name + "/ingredients";
 
       var stuff = parseZutaten(req.body.ingr);
-      console.log("Martinez: "+ JSON.parse(stuff));
+      console.log("Martinez: "+ JSON.stringify(stuff));
 
       var ingform = { url: newing, form: stuff };
 
