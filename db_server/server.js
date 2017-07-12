48df6728791fe3a9deadbca19cef2ba18a71c67e
NightmareEDM <lillykavsek@hotmail.com>
2017-07-12 19:27:12 +0200

    update server.js

--- db_server/server.js
+++ db_server/server.js
@@ -134,11 +134,9 @@ app.post("/createnewcocktail", jsonparser, (req, res) => {
       console.log("Error: " + error + "\nResponse: " + response + "\nBody: " + body);
 
       var stuff = parseZutaten(req.body.ingr);
+      var stuff = JSON.stringify(stuff);
+      console.log("Stuff: " + stuff);
       var ingform = { url: "http://127.0.0.1:" + DIENSTNUTZERPORT + "/cocktails/" + req.body.name + "/ingredients", form: stuff };
-
-      console.log("Ingform: " + JSON.stringify(ingform));
-
-      console.log("Stuff: " + JSON.stringify(stuff));
 
       request.post(ingform, (error1, response1, body1) => {
         res.send("Success");
